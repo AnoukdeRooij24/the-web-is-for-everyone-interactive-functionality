912816543354cb09436e119ee632427e6b30acd5
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-03-21 12:10:34 +0100

    Posten van comments werkt #7

--- server.js
+++ server.js
@@ -108,22 +108,29 @@ app.get('/webinars', async function (request, response) {
 // })
 
 app.get('/detail/:id', async function (request, response) { 
-  const webinarsdetailResponse = await fetch(`https://fdnd-agency.directus.app/items/avl_webinars/?fields=thumbnail,date,title,speakers,views,id&filter={"id":"${request.params.id}"}`)
+  const webinarsdetailResponse = await fetch(`https://fdnd-agency.directus.app/items/avl_webinars/?fields=thumbnail,date,video,duration,transcript,title,speakers.*.*,categories.*.*,views,id&filter={"id":"${request.params.id}"}`)
   const webinarsdetailResponseJSON = await webinarsdetailResponse.json()
+
+  const commentsResponse = await fetch(`https://fdnd-agency.directus.app/items/avl_comments/?fields=content,id`);
+  const commentsResponseJSON = await commentsResponse.json()
+
+  console.log(commentsResponseJSON)
 
 // console.log(webinarsdetailResponseJSON)
 
 response.render('detail.liquid', { 
-  webdetail: webinarsdetailResponseJSON.data[0] 
+  webdetail: webinarsdetailResponseJSON.data[0],
+  comments: commentsResponseJSON.data
+
 })
 
-console.log(webinarsdetailResponseJSON.data)
+// console.log(webinarsdetailResponseJSON.data)
 })
 
 
-/*
+
 // Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
-app.post(…, async function (request, response) {
+app.post('/detail/:id', async function (request, response) {
 
   // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
   console.log(request.body)
@@ -134,19 +141,22 @@ app.post(…, async function (request, response) {
   // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
   // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
   // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
-  await fetch(…, {
-    method: …,
-    body: JSON.stringify(…),
+  await fetch(`https://fdnd-agency.directus.app/items/avl_comments`, {
+    method: "POST",
+    body: JSON.stringify({
+      content: request.body.comment
+    }),
     headers: {
       'Content-Type': 'application/json;charset=UTF-8'
     }
+    
   });
 
   // Redirect de gebruiker daarna naar een logische volgende stap
   // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
-  response.redirect(303, …)
+  response.redirect(303,`/detail/${request.params.id}`)
 })
-*/
+
 
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
